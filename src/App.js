027c3816b2792977948506c4bec2f04cffdae20a
Owen Strength <Owenstrength@gmail.com>
2023-02-11 14:27:38 -0600

    Added Framer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
+import { motion } from "framer-motion";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <motion.div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </motion.div >
   );
 }
 
